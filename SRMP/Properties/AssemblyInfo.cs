a4847b13b5ddab282d92ec1545990d900e84f39d
Twirlbug <twirlbug@gmail.com>
2023-06-26 23:56:02 -0500

    Added last versionf of the runtime and json files to the libs folder. Rolled .net to match srml .net version

--- SRMP/Properties/AssemblyInfo.cs
+++ SRMP/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("0.0.0.1489")]
-[assembly: AssemblyFileVersion("0.0.0.1489")]
+[assembly: AssemblyVersion("0.0.0.1492")]
+[assembly: AssemblyFileVersion("0.0.0.1492")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
